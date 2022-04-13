fd142979af5a098f5f6eb0edc2b908531f6e0b8e
franc <onyishif@gmail.com>
2022-04-13 17:39:18 +0100

    hide and show layers

--- src/components/ShowMap.vue
+++ src/components/ShowMap.vue
@@ -9,9 +9,9 @@
                 <button class="border-2 border-gray-600 py-1 px-3 mt-5 hover:bg-red-300 bg-slate-300" @click="zoomOut">-</button>
             </div>
         </div>
-        <div class="mr-7 mt-4">
+        <div class="mr-7 mt-4 flex flex-col items-end">
             <div>
-                <input type="text" class="border-red-300 border-2 focus:outline-none py-1 px-1 focus:px-3 w-14 text-xs rounded focus:w-full focus:text-base peer" placeholder="Search ..." v-model="searchQuery"/>
+                <input type="text" class="border-red-400 border-2 focus:outline-none py-1 px-1 focus:px-3 w-14 text-xs rounded focus:w-full focus:text-base peer" placeholder="Search ..." v-model="searchQuery"/>
                 <div class="w-13 hidden peer-focus:block">
                     <div class="bg-white border mt-2">
                         <ul>
@@ -21,14 +21,30 @@
                     <hr>
                 </div>
             </div>
+            <div class="flex mt-32">
+                <div class="h-80 overflow-scroll bg-red-400 text-xs px-5" v-if="showLayers">
+                    <ul>
+                        <li v-for="(layerTypes, type) in sortedLayers" :key="type" class="group hover:font-medium hover:text-sm">{{type}}
+                            <ul class="hidden group-hover:block ml-4">
+                                <li v-for="layer in layerTypes" :key="layer.id" class="hover:font-bold cursor-pointer" @click="toggleLayer(layer.id)">
+                                    - {{layer.id}} | <span class="text-xs bg-green-400" v-if="layerState[layer.id]">show</span> <span v-else class="text-xs bg-red-700">hide</span>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <button class="border-2 border-gray-600 py-1 px-2 hover:bg-red-300 bg-slate-300 text-xs" @click="()=>showLayers = !showLayers">Layers</button>
+                </div>
+            </div>
         </div>
     </div>
     <div id="map" class="top-0 absolute bottom-0 w-full"></div>
 </template>
 <script setup lang="ts">
 import { onMounted, reactive, watchEffect } from '@vue/runtime-core';
-import { defineEmits, ref } from "vue"
-import mapboxgl,{ LngLatLike, Map } from 'mapbox-gl';
+import { computed, defineEmits, ref } from "vue"
+import mapboxgl,{ LngLatLike, Map, AnyLayer, Layer } from 'mapbox-gl';
 
 const emit = defineEmits<{
   (e: 'exitMap'): void
@@ -36,8 +52,46 @@ const emit = defineEmits<{
 
 const loading = ref(true);
 let map : Map;
+const layers =  ref<Layer[]>();
+const showLayers = ref(false)
 
 mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
+
+
+const sortedLayers = computed(() => {
+  let sortedStyles: Record<string, Layer[]> = {}
+        if(layers.value){
+            for(let mapStyle of layers.value){
+                if(!mapStyle['source-layer']){
+                    sortedStyles['general'] ? sortedStyles['general'].push(mapStyle): sortedStyles['general'] = [mapStyle]
+                    continue
+                    /* for(const key in Object.keys(sortedStyles)){
+                        console.log(key)
+                        let regex = new RegExp(mapStyle.id,'g')
+                        if(key.match(regex)){
+                            if(!sortedStyles[key]){
+                                sortedStyles[key] = [mapStyle];
+                                break;
+                            }
+                            sortedStyles[key].push(mapStyle);
+                            break;
+                        }
+                    } */
+                }
+                if(!sortedStyles[mapStyle['source-layer']]){
+                    sortedStyles[mapStyle['source-layer']] = [mapStyle]
+                    continue
+                }
+                sortedStyles[mapStyle['source-layer']].push(mapStyle)
+            }
+        }
+        return sortedStyles;
+})
+
+type LayerState = {
+    [index : Layer["id"]]: true | false
+}
+let layerState:LayerState = reactive({}) ;
 
 onMounted(()=>{
     map = new mapboxgl.Map({
@@ -46,8 +100,13 @@ onMounted(()=>{
         center: [-74.5, 40],
         zoom: 9 
     }); 
-    map.on('load',()=>{
-        console.log(map)
+    map.on('load', ()=>{
+        const mapLayers: any = map.getStyle().layers
+        // Set layer state
+        for(let layers of mapLayers){
+            layerState[layers.id] = false;
+        }
+        layers.value = mapLayers;
         loading.value = false;
     })
 })
@@ -58,6 +117,7 @@ function zoomIn(){
 function zoomOut(){
     map.zoomOut();
 }
+
 // search for a particular location
 let timeOut: number|null = null;
 const searchQuery = ref('');
@@ -86,6 +146,14 @@ function goToMap(location: LngLatLike){
     map.flyTo({
         center: location
     })
+}
+
+function toggleLayer(layer: Layer['id']){
+    const currentState = layerState[layer]
+    // if currentState = false then it is visible
+    const setAs = currentState ? 'visible' : 'none'
+    map.setLayoutProperty(layer,'visibility',setAs)
+    layerState[layer] = !currentState
 }
 </script>
 <style scoped>
